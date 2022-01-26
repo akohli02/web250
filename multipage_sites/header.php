fab82528b93dd6bd5cb30a52000a73a4f6441890
Aimy Kohli <78616209+akohli02@users.noreply.github.com>
2022-01-26 14:33:37 -0500

    Fixed toc validator

--- multipage_sites/header.php
+++ multipage_sites/header.php
@@ -6,6 +6,14 @@ echo '<header class="items">'.
 	 '<img id ="logo" alt = "Logo" src="images/logo.png" />'.
 	 '<div id="clock"></div>'.
 	 '<h1>Aimy Kohli WEB250</h1>'.
-	 '</header>';
+	 '<nav class = "links">'.
+	 '<ul>
+		<li><a href="toc.html">Table of Contents</a></li> 
+		<li> <a href="introduction.html">Introduction</a> </li>
+		<li><a href="contract.html">Contract</a></li>
+		<li><a href="brand.html">Brand</a></li>
+	</ul>'.
+	'</nav>'.
+	'</header>';
 
 ?>

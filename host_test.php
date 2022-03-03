ae68a08a7230513cee81261ac6de3fc4f239b93f
Aimy Kohli <78616209+akohli02@users.noreply.github.com>
2022-03-03 15:01:15 -0500

    Added screenshot and php file for testing localhost on my computer assignment

---
+++ host_test.php
@@ -0,0 +1,21 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$sOutputString = "<hr/>";
+
+
+for($iCounter = 1; $iCounter <= 1000; $iCounter++)
+{
+     $sOutputString .= $iCounter . ") Aimy got PHP running on her computer!! " . "<br/>";
+}
+
+
+echo ("<h5>" . $sOutputString . " </h5><hr/>");
+
+
+?>
+
+</body>
+</html> 

c373f378afa497a202a181926d9cc6c700b0f676
Aimy Kohli <78616209+akohli02@users.noreply.github.com>
2022-03-05 11:31:34 -0500

    Completed math, dates, and strings functions assignment

--- contents/toc.php
+++ contents/toc.php
@@ -25,6 +25,7 @@
 				<li><a href="car_shop/sams_used_cars.html">Joy of PHP Car Shop</a></li>
 				<li><a href="?p=form_results.php">One Form, Two forms, Red forms, Green Forms, two results, one page</a></li>
 				<li><a href="?p=everything_form.php">Everything Form</a></li>
+				<li><a href="?p=math_dates_strings.php">Math, Dates, and Strings - Sweet Paradise Specials</a></li>
 				<li><a href="?p=text_driven_website.php">Text Driven Website - Employee List</a></li>
 					
 			</ol> 

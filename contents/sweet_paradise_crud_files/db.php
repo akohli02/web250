59d55fa97c1b3bf731c19d241526e9272191c909
Aimy Kohli <78616209+akohli02@users.noreply.github.com>
2022-03-11 15:31:03 -0500

    Created a folder and files for sweet paradise log in and crud

---
+++ contents/sweet_paradise_crud_files/db.php
@@ -0,0 +1,11 @@
+ <?php
+$mysqli = new mysqli('sql301.epizy.com', 'epiz_30960597', 'MTYHj2muxW1', 'epiz_30960597_test' );
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+//select a database to work with
+$mysqli->select_db("epiz_30960597_test");
+ 
+?>
